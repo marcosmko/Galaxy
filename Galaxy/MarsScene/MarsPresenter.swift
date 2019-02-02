5a663f73ea69f8420b42418f0f0c938f58d32912
Marcos Kobuchi <marcos_kobuchi@hotmail.com>
2019-02-02 15:22:06 -0200

    Designs screen.

---
+++ Galaxy/MarsScene/MarsPresenter.swift
@@ -0,0 +1,17 @@
+//
+//  MarsPresenter.swift
+//  Galaxy
+//
+//  Created by Marcos Kobuchi on 02/02/19.
+//  Copyright © 2019 Marcos Kobuchi. All rights reserved.
+//
+
+import Foundation
+
+protocol MarsPresenterProtocol {
+    
+}
+
+class MarsPresenter: MarsPresenterProtocol {
+    weak var viewController: MarsViewController?
+}
---
+++ Galaxy/MarsScene/MarsRouter.swift
@@ -0,0 +1,17 @@
+//
+//  MarsRouter.swift
+//  Galaxy
+//
+//  Created by Marcos Kobuchi on 02/02/19.
+//  Copyright © 2019 Marcos Kobuchi. All rights reserved.
+//
+
+import Foundation
+
+protocol MarsRouterProtocol {
+    
+}
+
+class MarsRouter: MarsRouterProtocol {
+    weak var viewController: MarsViewController?
+}
